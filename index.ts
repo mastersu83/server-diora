05cdbe2900438a2a9c918d8844c5e93a3e4c3a41
mastersu83 <master_su@mail.ru>
2022-09-23 13:08:53 +0300

    fix

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import { router } from "./routes";
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
@@ -15,6 +16,8 @@ mongoose
 
 const app = express();
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "build")));
 
 app.use(cors());
 app.use("/uploads", express.static("uploads"));
